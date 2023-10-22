ec9b140da67177c67d8c956d02a243bad18776d8
Jori Hiltunen <hiltusenjori@gmail.com>
2023-10-22 16:18:45 +0200

    CREATE NODE.JS SERVER BACKEND 	modified:   .gitignore 	new file:   server/config.js 	new file:   server/index.js 	new file:   server/package-lock.json 	new file:   server/package.json 	new file:   server/rest.rest 	new file:   server/shoppinglist.sql

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2/promise')
+const config = require('./config')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+const port = 3001
+
+app.get('/', async (req, res) => {
+    try {
+        const conn = await mysql.createConnection(config.db)
+        const [result,] = await conn.execute('SELECT * FROM item')
+        if (!result) result = []
+
+        res.status(200).json(result)
+    } catch (err) {
+        res.status(500).json({ error: err.message })
+    }
+})
+
+app.listen(port)
---
+++ server/shoppinglist.sql
@@ -0,0 +1,21 @@
+DROP DATABASE IF EXISTS shoppinglist;
+
+CREATE DATABASE shoppinglist;
+
+USE shoppinglist;
+
+CREATE TABLE item (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    description VARCHAR(255) NOT NULL,
+    amount SMALLINT UNSIGNED NOT NULL
+);
+
+INSERT INTO
+    item (description, amount)
+VALUES
+    ('Test item', 1),
+    ('Second item', 5),
+    ('Third item', 32),
+    ('Fourth item', 7),
+    ('Fifth item', 99),
+    ('Sixth item', 56);
